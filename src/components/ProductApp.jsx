f5386b2e2090cdc884605e864d9bbf33acba6ea5
Miguel Quintana <miguelquintanadev@gmail.com>
2024-05-17 03:11:22 -0400

    ProductForm

--- src/components/ProductApp.jsx
+++ src/components/ProductApp.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { listProduct } from "../services/ProductService";
 import { ProductGrid } from "./ProductGrid";
 import { PropTypes } from 'prop-types'
+import { ProductForm } from "./ProductForm";
 
 export const ProductApp = ({title}) => {
 
@@ -13,10 +14,17 @@ export const ProductApp = ({title}) => {
     }, []);
     
     return (
-        <>
-        <h1>{title}</h1>
-        <ProductGrid products={products}/>
-        </>
+        <div>
+            <h1>{title}</h1>
+            <div>
+                <div>
+                    <ProductForm />
+                </div>
+                <div>
+                    <ProductGrid products={products}/>
+                </div>
+            </div>
+        </div>
     )
 }
 
