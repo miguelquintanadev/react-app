4312ab56b1cd4fdc6c1200823b8f8a0df0545b4e
Miguel Quintana <miguelquintanadev@gmail.com>
2024-05-18 22:08:46 -0400

    CRUD con BACK Spring

--- src/components/ProductApp.jsx
+++ src/components/ProductApp.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { listProduct } from "../services/ProductService";
+import { create, findAll, update } from "../services/ProductService";
 import { ProductGrid } from "./ProductGrid";
 import { PropTypes } from 'prop-types'
 import { ProductForm } from "./ProductForm";
@@ -15,31 +15,41 @@ export const ProductApp = ({title}) => {
         price: ''
     })
 
+    const getProducts = async () => {
+        const result = await findAll();
+        // console.log(result);
+        setProducts(result.data._embedded.products);
+    }
+    
     useEffect(() => {
-        const result = listProduct();
-        setProducts(result);
+        getProducts();
+        
     }, []);
     
     /* EL NUEVO OBJETO SE AÑADE A LOS QUE YA EXISTEN */
-    const handlerAddProduct = (product) => {
-        console.log(product);
-        
+    const handlerAddProduct = async (product) => {
+        // console.log(product);
         if(product.id > 0){
+            const response = await update(product);
             setProducts(products.map(prod => {
-                if(prod.id == product.id){
-                    return {...product}
+        
+                if(prod.id == response.data.id){
+                    return {...response.data}
                 }
                 return prod;
+                
             }));
         }else{
-            setProducts([...products, {...product, id: new Date().getTime()}])
+            const response = await create(product);
+            setProducts([...products, {...response.data}]);
         }
         
     }
 
     /* ELIMINAR OBJETO CON BOTON REMOVE. */
     const handlerRemoveProduct = (id) => {
-        console.log(id);
+        // console.log(id);
+        remove(id);
         setProducts(products.filter(product => product.id != id));
     }
 
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 
 const initProducts = [
     {
@@ -14,6 +15,54 @@ const initProducts = [
     }
 ];
 
-export const listProduct = () => {
+const baseUrl = 'http://localhost:8080/products';
+
+export const listProduct = async () => {
     return initProducts;
+}
+
+export const findAll = async () => {
+    try {
+        const response = await axios.get(baseUrl);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+    return null;
+}
+
+export const create = async ({name, description, price}) => {
+    try {
+        const response = await axios.post(baseUrl, {
+            name,
+            description,
+            price
+        });
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+    return undefined;  
+}
+
+export const update = async ({id, name, description, price}) => {
+    try {
+        const response = await axios.put(baseUrl + '/' + id, {
+            name,
+            description,
+            price
+        });
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+    return undefined;  
+}
+
+export const remove = async (id) => {
+    try {
+        await axios.delete(baseUrl + '/' + id);
+    } catch (error) {
+        console.log(error);
+    }
 }
